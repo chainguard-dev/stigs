2017c3243707a375a624df328d44dbc8c3a5a8e2
egibs <20933572+egibs@users.noreply.github.com>
2025-07-21 13:52:55 -0500

    Wrap script verifications in HTML tags; move pattern to single line
    Signed-off-by: egibs <20933572+egibs@users.noreply.github.com>

--- gpos/xml/scap/ssg/content/ssg-chainguard-xccdf/SupportingScripts/AslrCheck.sh
+++ gpos/xml/scap/ssg/content/ssg-chainguard-xccdf/SupportingScripts/AslrCheck.sh
@@ -4,7 +4,7 @@ aslr_value=$(sysctl kernel.randomize_va_space | awk '{print $3}')
 
 if [ $aslr_value -eq 2 ]; then
     return $XCCDF_RESULT_PASS
-else 
+else
     echo "ASLR not configured correctly"
     return $XCCDF_RESULT_FAIL
 fi
